434796e0a3d962d4b70d3e0c321087fdd270dafd
Sloth <jkasahara206@gmail.com>
2022-07-25 21:28:19 -0700

    changes to script.js

--- script.js
+++ script.js
@@ -3,59 +3,96 @@ var generateBtn = document.querySelector("#generate");
 
 
 // Assignment code here
-var lowerCase = ("abcdefghijklmnopqrstuvwxyz");
-var upperCase = ("ABCDEFGHIJKLMNOPQRSTUVWXZY");
-var number = ("0123456789");
-var special = (" !#$%&'()*+,-./:;<=>?@[]^_`{|}~");
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = "";
-  var passwordText = "";
-  let inputLength = prompt("Please choose how long you would like your password to be. It must be between 8 and 128 characters long.");
-  inputLength = parseInt(inputLength);
-
-  if (inputLength < 8){
-    alert("Password must be at leasst 8 characters.");
+function passwordLength() {
+    let passLength = prompt("Please enter desired password length. It must be between 8 and 128 characters long.");
+  if (passLength < 8){
+    alert("Password must be at least 8 characters.");
     return "";
   }
-  if (inputLength > 128) {
+  if (passLength > 128) {
     alert("Password may not exceed 128 characters.");
     return "";
   }
-  var inputLowerCase = confirm("Do you want lower case letters in your password?");
-  if (inputLowerCase) {
-      passwordText += lowerCase;
+  else {
+    console.log(`You want the password to be ${passLength} characters long`);
   }
-  var inputUpperCase = confirm("Do you want upper case letters in your password?");
-  if(inputUpperCase) {
-    passwordText += upperCase;
+  gencrit(passLength);
+}
+
+
+
+
+// Write password to the #password input
+function gencrit(pwLength) {
+  
+  let password = "";
+  let newPassword = "";
+
+  var lowerCase = ("abcdefghijklmnopqrstuvwxyz");
+  var upperCase = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+  var number = ("0123456789");
+  var special = (" !#$%&'()*+,-./:;<=>?@[]^_`{|}~");
+
+
+  let inputLowerCase = confirm("Do you want lower case letters in your password?");
+  let inputUpperCase = confirm("Do you want upper case letters in your password?");
+  let inputNumber = confirm("Do you want numbers in your password?");
+  let inputSpecial = confirm("Do you want special characters in your password?");
+
+  if (inputLowerCase == true) {
+      newPassword += lowerCase.charAt(Math.floor(Math.random() * lowerCase.length));
+      password = password.concat(lowerCase);
+      console.log("You want lower case values in your new password.");
   }
-  var inputNumber = confirm("Do you want numbers in your password?");
-  if(inputNumber) {
-    passwordText += number;
+
+
+  if(inputUpperCase == true) {
+    newPassword += upperCase.charAt(Math.floor(Math.random() * upperCase.length));
+    password = password.concat(upperCase);
+    console.log("You want upper case values in your new password.");
   }
-  var inputSpecial = confirm("Do you want special characters in your password?");
-  if(inputSpecial) {
-    passwordText += special;
+
+
+  if(inputNumber == true) {
+    newPassword += number.charAt(Math.floor(Math.random() * number.length));
+    password = password.concat(number);
+    console.log("You want numbers in your new password.");
   }
+
+
+  if(inputSpecial == true) {
+    newPassword += special.charAt(Math.floor(Math.random() * special.length));
+    password = password.concat(special);
+    console.log("You want special characters in your new password.");
+  }
+
+
+
   if (inputLowerCase + inputUpperCase + inputNumber + inputSpecial < 1) {
     alert("You must pick at least one parameter for your new password.");
     return "";
   }
-  for (var i = 0; i <inputLength; i++) {
-    password = passwordText [Math.floor(Math.random() * passwordText.length)];
+
+
+
+  for (var i = 0; i < pwLength; i++) {
+    newPassword += password.charAt(Math.floor(Math.random() * password.length));
   }
 
-function newPassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("password");
-    passwordText.value = password;
-}
-}
 
+  writePassword(newPassword);
+  
+}
+ 
+
+
+
+function writePassword(pw) {
+    var passwordText = document.querySelector("#password");
+    passwordText.value = pw;
+    console.log(`Your new password is ${pw}`);
+  }
 
 
 // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
+  generateBtn.addEventListener("click", passwordLength);
